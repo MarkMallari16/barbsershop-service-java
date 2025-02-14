087075b92a076a8baa0fb670ea62f1a5bbf38526
Mark Christian Mallari <104248911+MarkMallari16@users.noreply.github.com>
2025-02-14 10:14:19 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,13 +8,13 @@ public class Main {
         while (true) {
             Scanner sc = new Scanner(System.in);
 
-            System.out.print("Choose your 1st service: ");
+            System.out.print("Choose your 1st service (cut): ");
             String service1 = sc.nextLine();
 
-            System.out.print("Choose your 2nd service: ");
+            System.out.print("Choose your 2nd service (shave): ");
             String service2 = sc.nextLine();
 
-            System.out.print("Choose your 3rd service: ");
+            System.out.print("Choose your 3rd service (haircolor): ");
             String service3 = sc.nextLine();
 
             ThirdClass tc = new ThirdClass();
@@ -29,15 +29,22 @@ public class Main {
                 System.out.println("You chose: " + service1 + " " + service2);
                 tc.FirstChoice(true, true, false);
             } else if (service1.equals("") && service2.equals("shave") && service3.equals("haircolor")) {
+                System.out.println("You chose: " + service2 + " " + service3);
+                tc.FirstChoice(false, true, true);
+            } else if (service1.equals("") && service2.equals("shave") && service3.equals("haircolor")) {
+                System.out.println("You chose: " + service2 + " " + service3);
                 tc.FirstChoice(false, true, true);
             } else if (service1.equals("cut") && service2.equals("") && service3.equals("")) {
+                System.out.println("You chose: " + service2);
                 tc.FirstChoice(true, false, false);
-            } else if (service1.equals("") && service2.equals("shave") && service3.equals("haircolor")) {
-                tc.FirstChoice(true, true, true);
             } else if (service1.equals("") && service2.equals("shave") && service3.equals("")) {
                 tc.FirstChoice(false, true, false);
-                break;
+            } else if (service1.equals("") && service2.equals("") && service3.equals("haircolor")) {
+                tc.FirstChoice(false, false, true);
+            } else {
+                System.out.println("No service/invalid service found.");
             }
+            break;
         }
-
     }
+}
--- SecondClass.java
+++ SecondClass.java
@@ -1,8 +1,7 @@
-
 package barbershopservice;
 
-
 public class SecondClass {
+
     public static int cut = 100;
     public static int shave = 150;
     public static int hairColor = 200;
